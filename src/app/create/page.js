8278ac4a3c357ae8c940653b6cc268709a7f2c41
thedevpiyush <contactpiyushhere@gmail.com>
2025-01-09 22:26:37 +0530

    bug-fixed

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,314 +1,57 @@
-
-// Client-Side Code
-"use client";
-
-import React, { useState } from "react";
-import { CollectionCard } from "./Card";
-import { useToast } from "@/hooks/use-toast";
-
-export default function page() {
-
-    const { toast } = useToast();
-
-    const [files, setFiles] = useState([]);
-    const [urlList, setUrlList] = useState([]);
-    const [uploading, setUploading] = useState(false);
-    const [collectionName, setCollectionName] = useState("");
-
-    const uploadFiles = async () => {
-        try {
-            if (!files.length) {
-                toast({
-                    description: 'No files selected 📂'
-                })
-                return;
-            }
-
-            setUploading(true);
-            const data = new FormData();
-            files.forEach((file) => data.append("files", file));
-            data.set("collection", collectionName);
-
-            const uploadRequest = await fetch("/api/files", {
-                method: "POST",
-                body: data,
-            });
-
-            const ipfsUrls = await uploadRequest.json();
-            setUrlList(ipfsUrls);
-            setUploading(false);
-        } catch (e) {
-            console.error(e);
-            setUploading(false);
-            toast({
-                description: 'There was some problem uploading your Collection ☹️'
-            })
-        }
-    };
-
-    const handleFileChange = (e) => {
-        if (e.target?.files) {
-            setFiles(Array.from(e.target.files));
-        }
-    };
-
-    return (
-
-
-        // ----------------------------------------------------------------
-        // TO DO: COMPLETE UI OF THIS PAGE
-
-        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-        // COMPLETED : UPLOAD TO A COLLECTION IS NOW SET UP USING PINATA
-        // ----------------------------------------------------------------
-
-        <div className="w-full h-full">
-            <CollectionCard
-                collectionName={collectionName}
-                setCollectionName={setCollectionName}
-                file={files}
-                setFile={setFiles}
-                onSubmit={uploadFiles}
-            />
-
-        </div>
-    );
-}
-
-"use client";
-
-import React, { useState } from "react";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { CardWithForm } from "./Card";
-import { truncateAddress } from "@/lib/truncateAddress";
-import { useAccount } from "wagmi";
-
-export default function IntegratedNFTPage() {
-  const [files, setFiles] = useState([]);
-  const [urlList, setUrlList] = useState([]);
-  const [uploading, setUploading] = useState(false);
-  const [price, setPrice] = useState(0);
-  const [collectionName, setCollectionName] = useState("");
-  const [selectedFile, setSelectedFile] = useState(null);
-
-  const { address } = useAccount();
-
-  const uploadFiles = async () => {
-    try {
-      if (!files.length) {
-        alert("No files selected");
-        return;
-      }
-
-      setUploading(true);
-      const data = new FormData();
-      files.forEach((file) => data.append("files", file));
-      data.set("collection", collectionName);
-
-      const uploadRequest = await fetch("/api/files", {
-        method: "POST",
-        body: data,
-      });
-
-      const ipfsUrls = await uploadRequest.json();
-      setUrlList(ipfsUrls);
-      setUploading(false);
-    } catch (e) {
-      console.error(e);
-      setUploading(false);
-      alert("Trouble uploading files");
-    }
-  };
-
-  // const handleFileChange = (e) => {
-  //   if (e.target?.files) {
-  //     setFiles(Array.from(e.target.files));
-  //   }
-  // };
-
-  const calculatePrice = () => {
-    if (!price || isNaN(price)) return 0;
-    return (price * 0.99).toFixed(2);
-  };
-  const handleSingleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
-  };
-
-  const removeFile = () => {
-    setSelectedFile(null);
-  };
-
+import * as React from "react"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+export function CardWithForm() {
   return (
-    <div className="min-h-screen w-full bg-gray-900 text-white flex flex-col items-center p-8">
-      <div className="w-full max-w-3xl bg-gray-800 rounded-lg shadow-lg p-6">
-        <h1 className="text-4xl font-bold mb-4">Create New NFT</h1>
-
-        <div className="h-full w-full max-w-md bg-gray-800 border-gray-500 border rounded-lg shadow-lg p-4 flex flex-col sm:flex-row items-center gap-4 mb-6">
-          <div className="w-12 h-12 rounded-full bg-white flex items-center justify-center">
-            <img
-              src="https://s3.coinmarketcap.com/static-gravity/image/fd7a43cc620c4ade96804bb1c36aac6f.png"
-              alt="5ire Logo"
-              className="w-8 h-8"
-            />
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Create project</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="Name of your project" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="framework">Framework</Label>
+              <Select>
+                <SelectTrigger id="framework">
+                  <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  <SelectItem value="next">Next.js</SelectItem>
+                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                  <SelectItem value="astro">Astro</SelectItem>
+                  <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
           </div>
-          <div className="flex-1">
-            <p className="text-lg font-semibold">{truncateAddress(address)}</p>
-            <p className="text-sm text-gray-400">5ireChain</p>
-          </div>
-          <p className={`text-sm font-medium ${address ? "text-green-500" : "text-red-500"}`}>
-            {address ? "Connected" : "Not Connected"}
-          </p>
-        </div>
-
-        {/* Upload File Section */}
-        {/* <div className="mb-6">
-          <label className="block text-sm font-medium text-gray-400 mb-2">Upload Files</label>
-          <div className="border-2 border-dashed border-gray-600 rounded-lg p-4 flex flex-col items-center">
-            <input type="file" multiple onChange={handleFileChange} className="hidden" id="multi-file-input" />
-            <label
-              htmlFor="multi-file-input"
-              className="cursor-pointer bg-white hover:bg-slate-200 text-black py-2 px-4 rounded-lg"
-            >
-              Choose Files
-            </label>
-          </div>
-        </div> */}
-
-        {/* Single File Upload Section */}
-        <div className="mb-6">
-          <label className="block text-sm font-medium text-gray-400 mb-2">Upload Single File</label>
-          <div className="border-2 border-dashed border-gray-600 rounded-lg p-4 flex flex-col items-center">
-            <p className="text-gray-500 text-sm mb-2">PNG, GIF, WEBP, MP4, or MP3. Max 100mb.</p>
-            <input type="file" onChange={handleSingleFileChange} className="hidden" id="single-file-input" />
-            <label
-              htmlFor="single-file-input"
-              className="cursor-pointer bg-white hover:bg-slate-200 text-black py-2 px-4 rounded-lg"
-            >
-              Choose File
-            </label>
-            {selectedFile && (
-              <div className="mt-4 flex items-center gap-2">
-                <p className="text-green-400">{selectedFile.name}</p>
-                <button
-                  onClick={removeFile}
-                  className="bg-red-500 hover:bg-red-600 text-white w-8 h-8 flex items-center justify-center rounded-full text-sm"
-                >
-                  X
-                </button>
-              </div>
-            )}
-          </div>
-        </div>
-
-        <div className="mb-6">
-          <label htmlFor="price" className="block text-sm font-medium text-gray-400 mb-2">
-            Price
-          </label>
-          <input
-            id="price"
-            type="number"
-            placeholder="e.g. 2"
-            className="w-full bg-gray-700 text-white rounded-lg border border-gray-600 p-3"
-            onChange={(e) => setPrice(e.target.value)}
-          />
-        </div>
-
-        <div className="mb-6">
-          <Card className="h-full sm:h-40 max-w-md bg-gray-800 border-gray-500 rounded-3xl py-5 text-small sm:text-base">
-            <CardContent className="flex justify-between">
-              <p className="text-gray-400">Price per edition</p>
-              <p>{price || 0} 5IRE</p>
-            </CardContent>
-            <CardContent className="flex justify-between">
-              <p className="text-gray-400">Marketplace Fee</p>
-              <p> 1.0 %</p>
-            </CardContent>
-            <hr className="bg-white border-2"></hr>
-            <CardContent className="flex justify-between">
-              <p className="text-gray-400">You will receive</p>
-              <p>{calculatePrice()} 5IRE</p>
-            </CardContent>
-          </Card>
-        </div>
-
-        {/* Collection Name Input */}
-        <div className="mb-6">
-          <label htmlFor="collectionName" className="block text-sm font-medium text-gray-400 mb-2">
-            Collection Name
-          </label>
-          <input
-            id="collectionName"
-            type="text"
-            placeholder="e.g. 'Exclusive Art Collection'"
-            className="w-full bg-gray-700 text-white rounded-lg border border-gray-600 p-3"
-            onChange={(e) => setCollectionName(e.target.value)}
-          />
-        </div>
-
-        {/* Name Input */}
-        <div className="mb-6">
-          <label htmlFor="name" className="block text-sm font-medium text-gray-400 mb-2">
-            Name
-          </label>
-          <input
-            id="name"
-            type="text"
-            placeholder="e.g. 'Redeemable Tiger logo with a cup'"
-            className="w-full bg-gray-700 text-white rounded-lg border border-gray-600 p-3"
-          />
-        </div>
-
-        {/* Description Input */}
-        <div className="mb-6">
-          <label htmlFor="description" className="block text-sm font-medium text-gray-400 mb-2">
-            Description (Optional)
-          </label>
-          <textarea
-            id="description"
-            rows={4}
-            placeholder="e.g. 'After purchasing, you'll be able to get the real cup'"
-            className="w-full bg-gray-700 text-white rounded-lg border border-gray-600 p-3"
-          ></textarea>
-        </div>
-
-        {/* Royalties Input */}
-        <div className="mb-6">
-          <label htmlFor="royalties" className="block text-sm font-medium text-gray-400 mb-2">
-            Royalties
-          </label>
-          <input
-            id="royalties"
-            type="number"
-            placeholder="e.g. 12"
-            className="w-full bg-gray-700 text-white rounded-lg border border-gray-600 p-3"
-          />
-          <p className="text-gray-500 text-sm mt-2">Suggested: 0%, 10%, 20%, 30%. Maximum is 50%.</p>
-        </div>
-
-        {/* Upload Button */}
-        <button
-          type="button"
-          disabled={uploading}
-          onClick={uploadFiles}
-          className="w-full bg-white hover:bg-slate-200 text-black py-3 rounded-lg font-semibold mb-4"
-        >
-          {uploading ? "Uploading..." : "Upload Files"}
-        </button>
-
-        {/* Uploaded URLs */}
-        {/* <ul className="mt-4 text-sm text-gray-400">
-          {urlList.map((url, index) => (
-            <li key={index} className="mb-2">
-              <a href={url} target="_blank" rel="noopener noreferrer" className="text-blue-400 underline">
-                {url}
-              </a>
-            </li>
-          ))}
-        </ul> */}
-
-        {/* <CardWithForm /> */}
-      </div>
-    </div>
-  );
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
+  )
 }
