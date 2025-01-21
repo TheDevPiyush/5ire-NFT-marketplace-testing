66a6abba5249e9a8a6a784a05bd79ffdff30c246
thedevpiyush <contactpiyushhere@gmail.com>
2025-01-21 16:58:25 +0530

    bug-fix

--- src/components/CreatorsTableData.jsx
+++ src/components/CreatorsTableData.jsx
@@ -9,6 +9,7 @@ import {
     TableRow,
 } from "@/components/ui/table"
 import Link from 'next/link'
+import { truncateAddress } from '@/lib/truncateAddress'
 
 export default function CreatorsTableData({ data }) {
     return (
@@ -25,7 +26,7 @@ export default function CreatorsTableData({ data }) {
                     <TableRow key={index} >
                         <TableCell className="font-bold py-1 flex gap-3 items-center">
                             <span className='w-12 h-12 rounded-full overflow-hidden object-contain'><img className='w-fit object-contain' src={item?.profilePicUrl} alt="" /></span>
-                            <span className='text-lg'>{item?.walletAddress ? item.username : item.address}</span>
+                            <span className='text-lg'>{item?.walletAddress ? item.username > 20 ? truncateAddress(item.username) : item.username : truncateAddress(item.address)}</span>
                         </TableCell>
                     </TableRow>
                 ))}
